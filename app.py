24a255cdd54e9cba2688b87af5e608a0d28a931e
Akhash16 <testdummy862@gmail.com>
2020-10-13 01:55:26 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ from flask import Flask,render_template,request
 app = Flask(__name__,template_folder='templates')
 
 @app.route("/")
-def login():
+def home():
     return render_template("index.html")
 
-@app.route("/search")
-def login1():
-    return render_template("search.html")
+@app.route("/search.html")
+def search():
+    return render_template("search_v2.html")
+
+@app.route("/search_query",methods=['POST','GET'])
+def store():
+    query = request.form.get('user_query')
+    return query
+
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
